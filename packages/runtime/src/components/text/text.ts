167a58c7b57a69b0e52a05b34e6ead4acae388f1
flippedround <734243792@qq.com>
2026-07-22 20:52:54 +0800

    feat(text): add text component base style and style injection

--- packages/runtime/src/components/text/text.ts
+++ packages/runtime/src/components/text/text.ts
@@ -1,5 +1,8 @@
 import { createSlot, defineVaporComponent, setInsertionState, template } from "@vue/runtime-vapor";
+import { pushStyle } from "../../style.ts";
+import textCss from "./text.css?inline";
 
+pushStyle("minix:components", textCss);
 export default function Text() {
   const t = template(`<minix-text>`);
   return defineVaporComponent({
